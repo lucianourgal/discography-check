b6158bf3e95dbb1e42f3e9befdf42da804e34486
lucianourgal <lucianourgal@hotmail.com>
2019-10-09 23:18:37 -0300

    Software step by step

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 
 
 console.log("CHOCOLATE!!!!")
+
+// Reads folder location from configs.txt file
+
+// Reads all subfolders from selected folder
+
+// Treats first level folders as bands
+
+// Treats second and third level folders as albums
+
+// Extracts infos about found bands from metallum
+
+// Checks which albums are missing for each band
+
+// Generates a .html report
